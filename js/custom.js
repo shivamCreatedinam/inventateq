d3b8288cf99a5bce45ae19125e34f677a25b59cd
shubhamwadhwa711 <Swatimandal01@gmail.com>
2024-03-25 22:42:21 +0530

    testimonials & courses

--- js/custom.js
+++ js/custom.js
@@ -196,3 +196,67 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
     }
   });
 });
+
+
+
+/* 
+Courses */
+
+$(document).ready(function() {
+  $('.card-slider').slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 4,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
+
+
+/* testimonials */
+
+const swiperTestmonials = new Swiper('.swiper-testmonials', {
+  loop: true,
+  slidesPerView: 1.2,
+  grabCursor: true,
+  // Navigation arrows
+  navigation: {
+      nextEl: '.swiper-button-testmonials-next',
+      prevEl: '.swiper-button-testmonials-prev',
+  },
+  breakpoints: {
+      // when window width is >= 640px
+      500: {
+          slidesPerView: 1.4,
+      },
+      780: {
+          slidesPerView: 1.8,
+      },
+      1300: {
+          slidesPerView: 2.6,
+      },
+      1630: {
+          slidesPerView: 3.2,
+      }
+  }
+
+});
