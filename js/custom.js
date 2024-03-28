607bc55607a713c9d847ea54585ded850e930f97
shubhamwadhwa711 <Swatimandal01@gmail.com>
2024-03-28 19:26:36 +0530

    .

--- js/custom.js
+++ js/custom.js
@@ -165,6 +165,49 @@ function init() {
 
 init();
 
+/* About */
+
+class StepsComponent {
+  constructor(stepsSelector, contentsSelector) {
+    this.current = 1;
+    this.stepsNode = document.querySelector(stepsSelector);
+    this.contentsNode = document.querySelector(contentsSelector);
+
+    this.totalSteps = this.stepsNode.children.length;
+
+    this.stepsNode.querySelectorAll("button").forEach((step) => {
+      step.addEventListener("click", (e) => {
+        const targetStep = parseInt(e.target.dataset.step);
+
+        this.contentsNode
+          .querySelectorAll(".content")
+          .forEach((content) => content.classList.remove("active"));
+        this.stepsNode
+          .querySelectorAll("button")
+          .forEach((content) => content.classList.remove("active"));
+
+        this.contentsNode
+          .querySelector(`.content[data-step="${targetStep}"]`)
+          .classList.add("active");
+        this.stepsNode
+          .querySelector(`button[data-step="${targetStep}"]`)
+          .classList.add("active");
+        
+        this.stepsNode
+          .querySelectorAll("div")
+          .forEach((content) => content.classList.remove("active"));
+        if(targetStep - 1 > 0) {
+          const num = targetStep - 1;
+          for(let i = 1; i <= num; i++) {
+            this.stepsNode.querySelector(`div:nth-of-type(${i})`).classList.add('active');
+          }
+        }
+      });
+    });
+  }
+}
+
+new StepsComponent("#steps", "#contents");
 
 
 // Faq Accordion 
